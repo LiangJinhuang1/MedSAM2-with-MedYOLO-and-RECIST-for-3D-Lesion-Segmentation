8c0eb11c75b0fc67b4e624216e14e841d782ea46
Liang Jinhuang <162059831+LiangJinhuang1@users.noreply.github.com>
2025-08-27 16:59:05 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Script to download FLARE checkpoint
+# Script to download FLARE and MedYOLO checkpoints
 
 # Use either wget or curl to download the checkpoints
 if command -v wget > /dev/null 2>&1; then
@@ -26,3 +26,19 @@ else
     $CMD_WGET "$FLARE_DIR" "$FLARE_MODEL_URL" || { echo "Failed to download checkpoint from $FLARE_MODEL_URL"; exit 1; }
 fi
 echo "FLARE checkpoint has been downloaded successfully to the '${FLARE_DIR}' directory."
+
+# --- MedYOLO Checkpoint ---
+
+MEDYOLO_DIR="MedYOLO/runs/train/exp3/weights"
+mkdir -p "$MEDYOLO_DIR"
+
+MEDYOLO_MODEL_URL="https://huggingface.co/JinLemon/Seminar-Flare-2025/resolve/main/last.pt"
+MEDYOLO_MODEL_NAME="last.pt"
+
+echo "Downloading ${MEDYOLO_MODEL_NAME}..."
+if [ -n "$CMD_CURL" ]; then
+    $CMD_CURL "${MEDYOLO_DIR}/${MEDYOLO_MODEL_NAME}" "$MEDYOLO_MODEL_URL" || { echo "Failed to download checkpoint from $MEDYOLO_MODEL_URL"; exit 1; }
+else
+    $CMD_WGET "$MEDYOLO_DIR" "$MEDYOLO_MODEL_URL" || { echo "Failed to download checkpoint from $MEDYOLO_MODEL_URL"; exit 1; }
+fi
+echo "MedYOLO checkpoint has been downloaded successfully to the '${MEDYOLO_DIR}' directory."
